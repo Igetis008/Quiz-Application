90f75fe77e843f2295a489ce1f5edb1030abdc32
Igetis008 <aric01062008@gmail.com>
2025-08-25 16:42:40 +0530

    Add files via upload

---
+++ quizdb.sql
@@ -0,0 +1,24 @@
+
+
+-- Insert sample data
+INSERT INTO questions (category, question, option_a, option_b, option_c, option_d, answer)
+VALUES 
+('History', 'Who built the Buland Darwaza at Fatehpur Sikri?', 'Akbar', 'Shah Jahan', 'Babur', 'Aurangzeb', 'A'),
+
+('Science', 'What’s the universal speed limit in physics, measured in vacuum?', 'Speed of Sound', 'Speed of Light', 'Escape Velocity', 'Mach 1', 'B'),
+
+('Mythology', 'Who stole fire from the gods in Greek mythology?', 'Hermes', 'Prometheus', 'Apollo', 'Zeus', 'B'),
+
+('Tech', 'Which company’s logo was once a rainbow-colored apple?', 'Apple', 'Microsoft', 'Google', 'Atari', 'A'),
+
+('History', 'Who succeeded Winston Churchill as British PM in 1945?', 'Neville Chamberlain', 'Clement Attlee', 'Harold Macmillan', 'Anthony Eden', 'B'),
+
+('Maths', 'Which curve is defined as the set of all points equidistant from a fixed point and line?', 'Ellipse', 'Parabola', 'Hyperbola', 'Circle', 'B'),
+
+('Literature', 'Who wrote Animal Farm under a pen name?', 'Aldous Huxley', 'George Orwell', 'Mark Twain', 'J.K. Rowling', 'B'),
+
+('Space', 'Which was ISRO’s first satellite, launched in 1975?', 'Rohini', 'Aryabhata', 'Bhaskara', 'INSAT-1A', 'B'),
+
+('Chemistry', 'Which element has the atomic number 79?', 'Silver', 'Platinum', 'Gold', 'Copper', 'C'),
+
+('Pop Culture', 'Who won the first Academy Award for Best Actress in 1929?', 'Clara Bow', 'Mary Pickford', 'Janet Gaynor', 'Greta Garbo', 'C');
